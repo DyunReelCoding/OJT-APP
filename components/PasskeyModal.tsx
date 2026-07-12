d72600cf8495dbe3f415a2d125c9c5fca2693b98
Jonriel Baloyo <dyunreelcoding@gmail.com>
2026-07-12 15:09:01 +0800

    remove local storage for security

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import {
     AlertDialog,
     AlertDialogAction,
@@ -19,50 +19,33 @@ import {
   } from "@/components/ui/input-otp"
 
 import Image from 'next/image';
-import { usePathname, useRouter } from 'next/navigation';
-import { decryptKey, encryptKey } from '@/lib/utils';
+import { useRouter } from 'next/navigation';
 
 const PasskeyModal = () => {
     const router = useRouter();
-    const path = usePathname();
     const [open, setOpen] = useState(true);
     const [passkey, setPasskey] = useState('');
     const [error, setError] = useState('');
 
-    const encryptedKey = typeof window !== 'undefined' ? window.localStorage.getItem('accessKey') : null;
-
-    useEffect(() => {
-        const accessKey = encryptedKey &&  decryptKey (encryptedKey);
-        if(path){
-            if(accessKey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY){
-                setOpen(false);
-                router.push('/admin')
-            }else{
-                setOpen(true);
-            }
-        }
-        
-    }, [encryptedKey])
-
     const validatePasskey = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
 
-        if(passkey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY){
-            const  encryptedKey = encryptKey(passkey);
-
-            localStorage.setItem('accessKey', encryptedKey);
+        if (passkey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
             setOpen(false);
-        }else{
-            setError('Invalid passkey. Please try again.')
+            router.push('/admin');
+            return;
         }
+
+        setError('Invalid passkey. Please try again.')
     }
 
-    const closeModal = () =>{
+    const closeModal = () => {
         setOpen(false);
-        router.push('/')
+        router.push('/');
     }
-  return (
-    <AlertDialog open = {open} onOpenChange={setOpen}>
+
+    return (
+    <AlertDialog open={open} onOpenChange={setOpen}>
        
         <AlertDialogContent className='shad-alert-dialog'>
             <AlertDialogHeader>
